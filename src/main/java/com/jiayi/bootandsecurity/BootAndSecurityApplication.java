6219ef4536b162503214bfeeee4e485223e23a94
jiayi.nie <jiayi.nie@baozun.com>
2018-10-30 17:11:39 +0800

    a demo

---
+++ src/main/java/com/jiayi/bootandsecurity/BootAndSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.jiayi.bootandsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAndSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAndSecurityApplication.class, args);
+	}
+}
